3574922b91e04c87f2917effef9bc2fecf4c14f5
bancesroseline-alt <bancesroseline@gmail.com>
2026-05-17 19:47:39 -0600

    Update SecurityDisableConfig.java

--- src/main/java/com/proyecto/hce_backend/security/SecurityDisableConfig.java
+++ src/main/java/com/proyecto/hce_backend/security/SecurityDisableConfig.java
@@ -17,7 +17,7 @@ public class SecurityDisableConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .cors(cors -> {}) //agregado para front
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
@@ -35,4 +35,21 @@ public class SecurityDisableConfig {
 
         return http.build();
     }
+
+    @Bean
+    public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
+    org.springframework.web.cors.CorsConfiguration configuration =
+            new org.springframework.web.cors.CorsConfiguration();
+
+    configuration.addAllowedOrigin("https://hce-system.vercel.app");
+    configuration.addAllowedMethod("*");
+    configuration.addAllowedHeader("*");
+    configuration.setAllowCredentials(true);
+
+    org.springframework.web.cors.UrlBasedCorsConfigurationSource source =
+            new org.springframework.web.cors.UrlBasedCorsConfigurationSource();
+
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+}
 }
