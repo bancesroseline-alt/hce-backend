5031ffdd9df478cfed62c93ae16b38f6dc9788ef
bancesroseline-alt <bancesroseline@gmail.com>
2026-06-09 17:10:46 -0600

    Update SecurityDisableConfig.java

--- src/main/java/com/proyecto/hce_backend/security/SecurityDisableConfig.java
+++ src/main/java/com/proyecto/hce_backend/security/SecurityDisableConfig.java
@@ -3,13 +3,11 @@ package com.proyecto.hce_backend.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@Order(1)
 public class SecurityDisableConfig {
 
     @Autowired
