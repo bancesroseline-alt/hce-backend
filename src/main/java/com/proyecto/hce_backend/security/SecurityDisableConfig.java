c19896cbbbb4679c8d78c1d190f09512ac8be5ca
bancesroseline-alt <bancesroseline@gmail.com>
2026-06-09 16:51:42 -0600

    Update SecurityDisableConfig.java

--- src/main/java/com/proyecto/hce_backend/security/SecurityDisableConfig.java
+++ src/main/java/com/proyecto/hce_backend/security/SecurityDisableConfig.java
@@ -3,11 +3,13 @@ package com.proyecto.hce_backend.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@Order(1)
 public class SecurityDisableConfig {
 
     @Autowired
@@ -23,7 +25,7 @@ public class SecurityDisableConfig {
                 .formLogin(formLogin -> formLogin.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/api/dashboard/**").permitAll()
 
                         .requestMatchers("/api/usuarios/**").hasRole("ADMIN")
